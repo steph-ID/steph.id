fa5490f7c9f66f8f2ce5e1b7c0d9ac6704137310
Stephanie <106919710+steph-ID@users.noreply.github.com>
2022-07-23 03:49:19 +0000

    Actually changed photo URL

--- components/ProfilePhoto.jsx
+++ components/ProfilePhoto.jsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export default function ProfilePhoto() {
     return (
-        <img className="profile-photo" src="https://github.com/steph-ID/steph.id/blob/main/images/photo.png" alt="Professional headshot photo of Stephanie smiling" />
+        <img className="profile-photo" src="https://raw.githubusercontent.com/steph-ID/steph.id/main/images/photo.png" alt="Professional headshot photo of Stephanie smiling" />
     )
 }
