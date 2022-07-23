f7f99806893a09d2b2dde4b693157a02f5e10813
Stephanie <106919710+steph-ID@users.noreply.github.com>
2022-07-23 03:44:57 +0000

    Update for images

--- components/ProfilePhoto.jsx
+++ components/ProfilePhoto.jsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export default function ProfilePhoto() {
     return (
-        <img className="profile-photo" src="./images/photo.png" alt="Professional headshot photo of Stephanie smiling" />
+        <img className="profile-photo" src="../images/photo.png" alt="Professional headshot photo of Stephanie smiling" />
     )
 }
