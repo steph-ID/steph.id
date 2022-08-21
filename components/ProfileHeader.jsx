2bd54e341d21b97c06130f3011f8355101fa7f3f
Stephanie <connect@steph.id>
2022-08-21 19:44:37 -0400

    Update ProfileHeader.jsx

--- components/ProfileHeader.jsx
+++ components/ProfileHeader.jsx
@@ -4,7 +4,7 @@ export default function ProfileHeader() {
     return (
         <div className="profile-header">
             <h1>Stephanie Bledsoe</h1>
-            <h2>Aspiring Software Developer</h2>
+            <h2>Aspiring Software Engineer</h2>
             <p><a href="steph.id" target="_blank">steph.id</a></p>
         </div>
     )
